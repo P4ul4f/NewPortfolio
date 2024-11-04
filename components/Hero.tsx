47ab395166b6c0aa6e380d9db66e2c481e19df48
Paula Ferreyra <paulaferreyra24@gmail.com>
2024-11-03 22:33:50 -0300

    hero text update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Paula, , an Argentina-based developer dedicated to
+            Hi! I&apos;m Paula, an Argentina-based developer dedicated to
             building innovative digital solutions. solutions.
           </p>
 
