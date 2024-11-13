0ae261ff701af4bc5fcc516f952ca6673550970d
Paula Ferreyra <paulaferreyra24@gmail.com>
2024-11-13 17:43:52 -0300

    mobile fixes

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -5,7 +5,7 @@ import { CanvasRevealEffect } from "./ui/CanvasRevealEffect";
 
 const Approach = () => {
   return (
-    <section className="w-full py-20 sm:py-1">
+    <section className="w-full py-20 sm:pt-0">
       <h1 className="heading">
         My <span className="text-purple">approach</span>
       </h1>
